1cdedf8a494d57886d0445045c1d0d3a4baeb5b6
gseiten <shubhambhardwaj0826@gmail.com>
2020-04-29 21:03:22 +0530

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,172 @@
+
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="app">
+
+	<section class="hero is-info is-fullheight has-background is-transparent">
+		<div class="hero-head">
+		</div>
+		<div class="hero-body">
+			<div class="container has-text-centered">
+				<div class="column is-6 is-offset-3">
+					<h1 class="title">
+						Gseiten Autocomplete 101
+					</h1>
+					<h2 class="subtitle">
+						Search as you go...
+					</h2>
+					<div class="">
+						
+						<b-field label="">
+							<b-autocomplete
+								rounded
+								clearable
+								expanded
+								max-height="500px"
+								size="is-medium"
+								v-model="location_name"
+								:data="location_data"
+								field="name"
+								placeholder="e.g. delhi"
+								icon="magnify"
+								@typing="getAsyncData"
+								@select="option => selected_location = option">
+								<template slot="empty">No results found</template>
+
+								<template slot-scope="props">
+									<div class="is-inline-block">
+										<div class="has-text-weight-bold is-size-6">{{ props.option.name }}</div>
+										<div>{{ props.option.type }}, {{ props.option.state }}, {{ props.option.country }}</div>
+									</div>
+								</template>
+
+
+							</b-autocomplete>
+						</b-field>
+
+						<section class="box" v-if="selected_location">
+							<div class="content">
+								
+								<h3>
+									{{ selected_location['name'] }}, 
+									<span v-if="selected_location['city']"> {{ selected_location['city'] }},</span>
+									<span v-if="selected_location['postcode']"> {{ selected_location['postcode'] }},</span>
+									<span v-if="selected_location['state']"> {{selected_location['state']}},</span>
+									<span> {{selected_location['country']}}</span>
+									<br>
+									<span class="has-text-link">{{ selected_location['coordinates']['lat'] }}, {{ selected_location['coordinates']['lon'] }}</span>
+									<br>
+									<span class="is-italic has-text-info">{{ selected_location['type'] }}</span>
+								</h3>
+							</div>
+							
+						</section>
+
+
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+// import axios from 'axios';
+import debounce from 'lodash/debounce';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+
+	name: 'App',
+	components: {},
+	data() {
+		return {
+			location_name: "",
+			location_data: [],
+			selected_location: null,
+			isFetching: false,
+
+		}
+	},
+
+	methods: {
+
+		// getAsyncData: function(name){
+		// 	let letterCount = name.replace(/\s+/g, '').length;
+		// 	if(letterCount > 3){
+		// 		axios.get(`http://photon.komoot.de/api/?q=${name}&limit=8`).then(response => {
+		// 			this.location_data = [];
+		// 			for(var i in response.data['features']){
+		// 				this.location_data.push(
+		// 					{
+		// 						'name': response.data['features'][i]['properties']['name'],
+		// 						'country':  response.data['features'][i]['properties']['country'],
+		// 						'type': response.data['features'][i]['properties']['osm_value'],
+		// 						'state': response.data['features'][i]['properties']['state']
+		// 					}
+		// 				)
+		// 			}
+		// 	})
+		// 	}
+		// }
+
+		getAsyncData: debounce(function (name) {
+			if (!name.length) {
+				this.location_data = []
+				return
+			}
+			this.isFetching = true
+			this.$http.get(`http://photon.komoot.de/api/?q=${name}&limit=8`)
+				.then(({ data }) => {
+					this.location_data = []
+					data.features.forEach((item) => this.location_data.push(
+						{
+							'name': item.properties.name,
+							'country': item.properties.country,
+							'type': item.properties.osm_value,
+							'state': item.properties.state,
+							'postcode': item.properties.postcode,
+							'city': item.properties.city,
+							'coordinates': {
+								'lat': item.geometry.coordinates[0],
+								'lon': item.geometry.coordinates[1]
+							}
+						}
+					))
+				})
+				.catch((error) => {
+					this.location_data = []
+					throw error
+				})
+				.finally(() => {
+					this.isFetching = false
+				})
+		}, 250)
+
+	}
 }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+.hero.has-background {
+    background: url('./assets/background.jpg');
+    position: relative;
+    overflow: hidden;
 }
+
+.hero-background {
+    position: absolute;
+    object-fit: cover;
+    object-position: center center;
+    width: 100%;
+    height: 100%;
+}
+
+/* .hero-background.is-transparent {
+    opacity: 0.8;
+} */
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'bulma/css/bulma.css'
+
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+
+import Buefy from 'buefy';
+import 'buefy/dist/buefy.css';
+Vue.use(Buefy);
+
+import VueResource from 'vue-resource'
+Vue.use(VueResource)
 
 Vue.config.productionTip = false
 
